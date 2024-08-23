dfb72efb3ab4f619c823cd8762455148b5cc1e10
ahxuan <xuan45678901@gmail.com>
2024-08-23 15:25:26 +0800

    up

--- src/hw1/Hw.java
+++ src/hw1/Hw.java
@@ -1,6 +1,6 @@
 package hw1;
 
-public class hw1 {
+public class Hw {
 
 	public static void main(String[] args) {
 
--- src/hw2/Hw.java
+++ src/hw2/Hw.java
@@ -1,6 +1,6 @@
 package hw2;
 
-public class hw2 {
+public class Hw {
 	public static void main(String[] args) {
 
 		// 1. 計算 1~1000 的偶數和 (2+4+6+8+...+1000)
@@ -24,7 +24,7 @@ public class hw2 {
 		for (int s = 1; s <= 10; s++) { // 從 1 到 10 的迴圈
 			System.out.print(s * s + " "); // 計算平方並輸出
 		}
-		System.out.println(); // 換行
+		System.out.println();
 
 		// 4.阿文很熱衷大樂透 (1 ~ 49),但他不喜歡有4的數字,不論是個位數或是十位數。請設計一隻程式,
 		// 輸出結果為阿文可以選擇的數字有哪些?總共有幾個?
@@ -56,42 +56,39 @@ public class hw2 {
 		System.out.println("阿文可以選擇的數字有" + res);
 		System.out.println("總共有幾個" + Integer.toString(count));
 
-		// 5.請設計一隻Java程式,輸出結果為以下:
-//		1 2 3 4 5 6 7 8 9 10
-//		1 2 3 4 5 6 7 8 9
-//		1 2 3 4 5 6 7 8
-//		1 2 3 4 5 6 7
-//		1 2 3 4 5 6
-//		1 2 3 4 5
-//		1 2 3 4
-//		1 2 3
-//		1 2
-//		1
-//	
+		// 5. 請設計一隻Java程式,輸出結果為以下:
+				// 1 2 3 4 5 6 7 8 9 10
+				// 1 2 3 4 5 6 7 8 9
+				// 1 2 3 4 5 6 7 8
+				// 1 2 3 4 5 6 7
+				// 1 2 3 4 5 6
+				// 1 2 3 4 5
+				// 1 2 3 4
+				// 1 2 3
+				// 1 2
+				// 1
+				System.out.println("5. ");
+				for (int i = 10; i >= 1; i--) { // 從 10 到 1 的外層迴圈
+					for (int j = 1; j <= i; j++) { // 內層迴圈從 1 到 i
+						System.out.print(j + " "); // 輸出當前的 j 值
+					}
+					System.out.println(); // 每次內層迴圈結束後換行
+				}
 
-		System.out.println("5. ");
-		for (int i = 10; i >= 1; i--) { // 從 10 到 1 的外層迴圈
-			for (int j = 1; j <= i; j++) { // 內層迴圈從 1 到 i
-				System.out.print(j + " "); // 輸出當前的 j 值
+				// 6. 請設計一隻Java程式,輸出結果為以下:
+				// A
+				// BB
+				// CCC
+				// DDDD
+				// EEEEE
+				// FFFFFF
+				System.out.println("6. ");
+				for (int i = 1; i <= 6; i++) {
+					char ch = (char) ('A' + i - 1);
+					for (int j = 1; j <= i; j++) { // 內層迴圈，控制字母的重複次數
+						System.out.print(ch); 
+					}
+					System.out.println(); 
+				}
 			}
-			System.out.println(); // 每列輸出完成後換行
 		}
-
-//		6. 請設計一隻Java程式,輸出結果為以下:
-//			A
-//			BB
-//			CCC
-//			DDDD
-//			EEEEE
-//			FFFFFF
-
-		System.out.println("6. ");
-		for (int i = 1; i <= 6; i++) {
-			char ch = (char) ('A' + i - 1); // 計算當前的字母
-			for (int j = 1; j <= i; j++) { // 內層迴圈，控制字母的重複次數
-				System.out.print(ch); // 輸出字母
-			}
-			System.out.println(); // 換行
-		}
-	}
-}
